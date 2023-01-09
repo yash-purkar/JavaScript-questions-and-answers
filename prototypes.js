9b9e347f1f629dd19eabd95ccb3bee2523d54eed
yashpurkar <yashpurkar7079@gmail.com>
2023-01-09 12:40:29 +0530

    update

--- prototypes.js
+++ prototypes.js
@@ -1,44 +1,88 @@
-// In javaScript every object has built-in property, which is called prototype.Prototype is an object itself. so every prototype will have its own prototype.
+// Prototypes
+// Everything in JS is an object.
+
+// In javaScript every object has built -in property, which is called prototype.A prototype is an object itself.so every prototype will have its own prototype.
 
 // All the objects inherit the properties or methods from prototype.
 
+//   example
 
-// e.g
 const obj1 = {
-  alpha: "A",
-  getAlpha: function () {
-    console.log(this.alpha);
+  name: "Kunal",
+  rollNo: 1,
+  getName: function () {
+    return this.name;
   },
-  getNum: function () {
-    console.log(this.num);
+  getAge: function () {
+    let currentYear = new Date().getFullYear();
+    return currentYear - this.birth;
   }
 }
 
 const obj2 = {
-  num: 1,
+  rollNo: 2,
+  birth: 2003,
   __proto__: obj1
 }
 
-console.log(obj2.getAlpha());
-console.log(obj2.getNum());
+console.log(obj2.name);
+console.log(obj2.rollNo);
+console.log(obj2.getAge());
 
-// output will be 'A' and '1'
+// In above e.g we can see, we logged 'obj2.name' and it printed Kunal that is in obj1.
 
-// suppose in obj2 we have 'alpha' too, then it will print the alpha of obj2.
-// It prints nearest.
+// 1st it found 'name' in 'obj2' and didn't get and then it went inside the 'obj1' prototype.
+
+// If we can try to access property which is not in any prototype then it prints undefined.
+
+// And we also logged 'obj2.rollNo' and printed '2' that is in 'obj2' but the 'obj1' also has the 'rollNo', it printed '2' that is in 'obj2' because It prints nearest.
+
+// We can use all methods of prototypes, just like we use here getAge() method of 'obj1'.
+
+// Here we use __proto__ and it creates the new prototype of given object and we can use methods of that object.
+
+
+// Here we can see '2' prototypes in 'obj2'.
+
+//   The 1st prototype is of 'obj1'.We can see all methods of 'obj1'.
+
+//     The 2nd prototype is for 'obj2' becasue every object has it's own prototype.
+
+// Prototypes of an Arrays.
+// Array also has it's prototype. And it's derives from the object prototype.
+
+//   example
+
+// const arr = ["Yash"];
+// console.log(arr);
+// output
+
+
+// We can see here array has it's own prototype.
+
+// And inside the array prototype we can see methods of an array.like map(), filter() etc.
+
+// And we can use that methods easily.
+
+// We can also add a method / property in prototype.
+
+// Array.prototype.show = function () {
+//   return this;
+// }
+// Here we added a method in prototype, that does nothing but returns the given array.
 
 
 
-// Creating a prototype of array
+// Now we can see show() method in an array prototype.
 
-Array.prototype.sum = function () {
-  let total = 0;
-  this.forEach(currElem => total += currElem);
-  return total;
-}
+// We can use this method like we use another methods of an array.
 
-const numbers = [1, 2, 3, 4, 5, 6];
-console.log(numbers.sum());
 
-// We have created method in array prototype like map(), filter(),
-// we created sum(), which returns the sum of elements of the array.
+
+
+
+// const names = ["Yash", "Arjun"];
+// console.log(names.show());
+
+
+// It printed an names array, becasue in show() method we are only returning an array, not did any operation.
