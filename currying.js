c8054c274242060b946253adc1dcce0ede087dcf
yashpurkar <yashpurkar7079@gmail.com>
2023-01-10 21:44:32 +0530

    updated currying

--- currying.js
+++ currying.js
@@ -1,47 +1,57 @@
-// Normal function
-// function add(a, b) {
-//   return a + b;
-// }
+// Currying is a function that takes one argument at a time and returns a new function expecting the next argument.
 
-// console.log(add(5, 4));
+// Without currying
 
-// Problem stmt.
-function add(a) {
+function currying(a) {
   return function (b) {
     return function (c) {
       return a + b + c;
     }
   }
 }
+//  without currying
+let a1 = currying(5);
+// console.log(a1);
 
-/* without currying
-let a1 = add(5);
-console.log(a1);
+let a2 = a1(7);
+// console.log(a2);
 
-let a2 = a1(5);
-console.log(a2);
-
-let a3 = a2(5);
-console.log(a3);
-*/
-
-// We did this but it's not a right way.
+let a3 = a2(3);
+// console.log(a3);
 
 
-// With Currying.
-let res = add(5)(5)(5);
-// console.log(res);
+
+// We can see in console the currying function is returning a function and that function also returning the another function and inside this we are returning sum of the numbers.
+
+// So We can see if we pass an argument to currying() it is returning an another function we stored that function inside a1;
+// So that another function is taking an argument , no we stored that function in a1, so we passed the argument to a1. Now that function was called, and that this function is also returning an another function so we stored that another function in a2, And now this function is returning the sum of the numbers.
+// i.e 15
+
+
+
+// BUT THIS IS NOT RIGHT WAY TO DO THAT.
+
+// We can do by simple way using currying;
+
+let total = currying(5)(7)(3);
+// console.log(total);
+
+// Now the 5 will assign to the a, now the currying function returning the another function and that function is taking an argument so the 7 will assign to b, now this function is also returning the another function and taking ane one argument so 3 will assign to c.
+// Then c will return the sum of the numbers that will be 15/
+
 
 
 
 
 
 // INFINITE CURRYING
+// e.g
+// If we pass infinite number of nested function argument
 
-function addition(a) { //7
+function currying(a) { //2|7|13|15|16
   return function (b) {
     if (b) {
-      return addition(a + b); //2+5=7|7+6=13|13+2=15|15+1=16
+      return currying(a + b); //2+5=7|7+6=13|13+2=15|15+1=16
     }
     else {
       return a; //16
@@ -49,49 +59,36 @@ function addition(a) { //7
   }
 }
 
-// console.log(addition(2)(5)(6)(2)(1)()) //16
+// console.log(currying(2)(5)(6)(2)(1)()) //16
+
+
+
+// 1st We pass 2 that will assign to a then the currying is returning the function and that function is taking an one argument so we pass the 5. Now that function will check, Is any value inside b? 
+// In case of yes it will call the currying function again and that function is taking an argument so we pass the argument a+b that will be 2+5=7 so now a will be 7 and the currying is returning the function and that function expects the one argument and we pass the 6 this time and it will check Is any value inside b? and so on.
+
+// And once it'll see the () empty, that means no value in b then it will return the value of a.
 
 
 // Function Currying is a process in functional programming in which we transform function with multiple arguments into a series of nested functions that takes one argument and eventually resolve to a value.
 
 
-// function sum(a) {
-//   return function (b) {
-//     return function (c) {
-//       console.log(a, b, c);
-//       return a + b + c;
-//     }
-//   }
-// }
-
-// console.log(sum(5)(4)(1));
-
-// function evaluate(a) {
-//   return function (b) {
-//     return function (c) {
-//       if (a == "sum") {
-//         return b + c;
-//       }
-//       else if (a == "sub") {
-//         return b - c;
-//       }
-//     }
-//   }
-// }
-
-// console.log(evaluate("sub")(5)(10))
 
 
-function sum(a) { //50 //60 //65
-  return function (b) { //10
-    if (b) { //true //true
-      return sum(a + b); //60 //60+5 =65
-    }
-    else {
-      return a;
+function evaluate(a) {
+  return function (b) {
+    return function (c) {
+      if (a == "sum") {
+        return b + c;
+      }
+      else if (a == "sub") {
+        return b - c;
+      }
     }
   }
 }
+console.log(evaluate("sum")(100)(50));
+console.log(evaluate("sub")(100)(50));
 
-let total = sum(50)(10)(5)();
-// console.log(total);
+
+// We are passing an operation name to the evaluate() function and this function is returning an another function which expects the one argument , and that function is also returning the another function which also expects the one argument and then it returns the result on conditions.
+
